fb7c4c1de1c76e86a4abbcbe230394b19c99dcfc
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-03-18 10:18:04 +0530

    Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,19 @@ plugins {
 
 android {
     namespace = "com.example.goodwill"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.goodwill"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 
     buildTypes {
