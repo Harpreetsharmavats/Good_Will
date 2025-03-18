6263049106210ad3369e2a47c74eaccc10f7babd
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-03-18 18:55:42 +0530

    Changes

--- app/src/main/java/com/example/goodwill/LoginActivity.kt
+++ app/src/main/java/com/example/goodwill/LoginActivity.kt
@@ -1,15 +1,22 @@
 package com.example.goodwill
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.goodwill.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.continueAsContributor.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
 
     }
 }
