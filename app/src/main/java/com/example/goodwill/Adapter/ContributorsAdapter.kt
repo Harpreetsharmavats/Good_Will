5ea47ee57b73b6760a1aa837532c975c08908aa0
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-03-22 16:22:39 +0530

    Changes

--- app/src/main/java/com/example/goodwill/Adapter/ContributorsAdapter.kt
+++ app/src/main/java/com/example/goodwill/Adapter/ContributorsAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.goodwill.Adapter
 
-class ContributorsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.goodwill.Models.ContributorDetails
+import com.example.goodwill.databinding.ContributorslayoutBinding
+
+class ContributorsAdapter (context: Context,private val details :List<ContributorDetails>) : RecyclerView.Adapter<ContributorsAdapter.ContributorViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContributorViewHolder {
+        return ContributorViewHolder(ContributorslayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int = details.size
+
+    override fun onBindViewHolder(holder: ContributorViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class ContributorViewHolder(private val binding: ContributorslayoutBinding) : ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.name.text = details[position].name
+            binding.phoneNumber.text = details[position].phoneNumber
+            binding.address.text = details[position].address
+            binding.amount.text = details[position].amount
+            binding.eventName.text = details[position].event
+        }
+
+    }
 }
