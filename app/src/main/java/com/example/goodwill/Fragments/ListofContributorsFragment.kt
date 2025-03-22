811cca97e6b468cc027c2ed35b64926ae4e417e4
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-03-22 18:27:18 +0530

    Changes

--- app/src/main/java/com/example/goodwill/Fragments/ListofContributorsFragment.kt
+++ app/src/main/java/com/example/goodwill/Fragments/ListofContributorsFragment.kt
@@ -5,11 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.goodwill.Adapter.ContributorsAdapter
+import com.example.goodwill.Models.AddEventDetails
+import com.example.goodwill.Models.ContributorDetails
 import com.example.goodwill.R
+import com.example.goodwill.databinding.FragmentListofContributorsBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class ListofContributorsFragment : Fragment() {
-
+    private lateinit var binding : FragmentListofContributorsBinding
+private lateinit var database : FirebaseDatabase
+private lateinit var contributorsList : ArrayList<ContributorDetails>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,8 +32,35 @@ class ListofContributorsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentListofContributorsBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listof_contributors, container, false)
+        showContributors()
+        return binding.root
+    }
+    private fun showContributors() {
+        database = FirebaseDatabase.getInstance()
+        contributorsList = ArrayList()
+        val eventRef: DatabaseReference = database.reference.child("Events")
+        eventRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (eventSnapshot in snapshot.children){
+                    val events = eventSnapshot.getValue(ContributorDetails::class.java)
+                    events?.let { contributorsList.add(it) }
+                    setAdapter()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), "Loading Error", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun setAdapter() {
+        val adapter = ContributorsAdapter(requireContext(),contributorsList)
+        binding.recyclerView.layoutManager =LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
     }
 
     companion object {
